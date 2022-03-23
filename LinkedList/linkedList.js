6f8d3701ebf71a2e71e51f3ebdc8b5e412dd16b6
sk2597 <smruti.karade25@gmail.com>
2022-03-24 00:50:09 +0530

    insert and remove a node from an Index

--- LinkedList/linkedList.js
+++ LinkedList/linkedList.js
@@ -38,8 +38,40 @@ class LinkedList {
     console.log({list});
     console.log("Length = ",this.length);
   }
+
+  traverseToIndex(index){
+    let counter = 0;
+    let currentNode = this.head;
+    while(counter !== index){
+      currentNode = currentNode.next;
+      counter++
+    }
+    return currentNode;
+  }
+
+  insertAtIndex(index, value){
+    if(index >= this.length){
+      this.insertNodeAtTheEnd(value);
+    }else {
+      let newNode = new Node(value);
+      let prevNode = this.traverseToIndex(index-1);
+      newNode.next = prevNode.next;
+      prevNode.next = newNode;
+      this.length++
+      this.printList();
+    }
+  }
+
+  removeNodeAtIndex(index){
+    let prevNode = this.traverseToIndex(index-1);
+    prevNode.next = prevNode.next.next;
+    this.length--;
+    this.printList();
+  }
 }
 
 const myLinkedList = new LinkedList(10);
 myLinkedList.insertNodeAtTheEnd(15);
 myLinkedList.insertNodeAtTheBegining(5);
+myLinkedList.insertAtIndex(2,12);
+myLinkedList.removeNodeAtIndex(2);
