40379fff72f5f696f32830a4e48684f358a8031b
sk2597 <smruti.karade25@gmail.com>
2022-03-23 23:04:40 +0530

    basic implementation of linkedlist class, prepend and append

--- LinkedList/linkedList.js
+++ LinkedList/linkedList.js
@@ -1,1 +1,37 @@
-console.log("hello World");
+class LinkedList {
+  constructor(value){
+    this.head = {
+      value: value,
+      next: null
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  insertNodeAtTheEnd(value) {
+    const newNode = {
+      value: value,
+      next: null
+    }
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++
+    return this;
+  }
+
+  insertNodeAtTheBegining(value){
+    const newNode = {
+      value: value,
+      next: this.head
+    }
+    this.head = newNode;
+    this.length++
+    return this;
+  }
+}
+
+const myLinkedList = new LinkedList(10);
+myLinkedList.insertNodeAtTheEnd(15);
+myLinkedList.insertNodeAtTheBegining(5);
+console.log({myLinkedList});
+console.log(myLinkedList.head);
